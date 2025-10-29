a5d5d1e994b8d5f8803696b94c20a98c5f4d8319
Vedaant Adavikolanu <avedaant@gmail.com>
2025-10-28 20:07:58 -0700

    Added single-frame task

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/Intake.java
@@ -3,29 +3,51 @@ package org.firstinspires.ftc.teamcode.subSystems;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.jumpypants.murphy.RobotContext;
 import com.jumpypants.murphy.tasks.Task;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MyRobot;
 public class Intake {
     private final Motor IntakeMotor;
-    private final Gamepad gamepad1;
 
     private static int buttonClick;
 
-    public Intake(HardwareMap hardwareMap, Gamepad gamepad1) {
+    public Intake(HardwareMap hardwareMap) {
         IntakeMotor = new Motor(hardwareMap, "IntakeMotor");
         IntakeMotor.setRunMode(Motor.RunMode.RawPower);
         IntakeMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         buttonClick = 0;
 
-        this.gamepad1 = gamepad1;
     }
 
-    public class runIntakeMotor extends Task {
+    public class AutonIntakeMotor extends Task {
+        private final double TARGET_POSITION; 
+        public AutonIntakeMotor(RobotContext robotContext, double targetPosition) {
+            super(robotContext);
+            TARGET_POSITION = targetPosition;
 
-        public runIntakeMotor(RobotContext robotContext, double targetPosition) {
+        }
+
+        @Override
+        protected void initialize(RobotContext robotContext) {
+            IntakeMotor.set(0);
+        }
+
+        @Override
+        protected boolean run(RobotContext robotContext) {
+            double currentPos = IntakeMotor.getCurrentPosition();
+            if (Math.abs(TARGET_POSITION-currentPos)<10) {
+                IntakeMotor.set(0);
+                return true;
+            }
+            IntakeMotor.set(1);
+            return false;
+        }
+    }
+
+    public class TeleOpIntake extends Task {
+
+        public TeleOpIntake (RobotContext robotContext, double targetPosition) {
             super(robotContext);
         }
 
