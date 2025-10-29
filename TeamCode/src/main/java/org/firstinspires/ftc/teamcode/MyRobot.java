417a6aab54431c10305dc356d33d7ab895cc4867
Abhi2009CU <146779945+Abhi2009CU@users.noreply.github.com>
2025-10-28 19:05:59 -0700

    update MyRobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyRobot.java
@@ -5,12 +5,20 @@ import com.jumpypants.murphy.RobotContext;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subSystems.Intake;
+import org.firstinspires.ftc.teamcode.subSystems.Launcher;
+import org.firstinspires.ftc.teamcode.subSystems.Transfer;
+import org.firstinspires.ftc.teamcode.subSystems.Turret;
 
 /**
  * MyRobot class that extends RobotContext to include robot-specific subsystems.
  */
 public class MyRobot extends RobotContext {
     public final MecanumDrive DRIVE;
+    public final Intake INTAKE;
+    public final Launcher LAUNCHER;
+    public final Transfer TRANSFER;
+    public final Turret TURRET;
 
     //not sure about the following
     public Gamepad gamepad1, gamepad2;
@@ -23,9 +31,13 @@ public class MyRobot extends RobotContext {
      * @param gamepad1  the primary gamepad controller
      * @param gamepad2  the secondary gamepad controller
      */
-    public MyRobot(Telemetry telemetry, Gamepad gamepad1, Gamepad gamepad2, MecanumDrive drive) {
+    public MyRobot(Telemetry telemetry, Gamepad gamepad1, Gamepad gamepad2, MecanumDrive drive, Intake intake, Launcher launcher, Transfer transfer, Turret turret) {
         super(telemetry, gamepad1, gamepad2);
         this.DRIVE = drive;
+        this.INTAKE = intake;
+        this.LAUNCHER = launcher;
+        this.TRANSFER = transfer;
+        this.TURRET = turret;
 
         //not sure about the following
         this.gamepad1 = gamepad1;
